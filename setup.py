b2974e6973ee0a043e8116f9ac546be3b92fc884
cociweb <cociweb@users.noreply.github.com>
2023-11-30 23:01:10 +0100

    Update setup.py
    Add support for 0.0.10

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="epsonprinter",
-    version="0.0.7",
+    version="0.0.10",
     author="ThaStealth",
     author_email="author@example.com",
     description="Communication package for Epson Workforce printer",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/thastealth/epsonprinter",
+    url="https://github.com/cociweb/epsonprinter",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
