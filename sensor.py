65c9c00ebe94252f669252d00f2818b1da178561
cociweb <cociweb@users.noreply.github.com>
2023-11-30 23:10:23 +0100

    Update sensor.py

--- sensor.py
+++ sensor.py
@@ -1,92 +1,116 @@
-import logging
+"""Support for Epson Workforce Printer."""
+from __future__ import annotations
 
 from datetime import timedelta
 
-
+from epsonprinter_pkg.epsonprinterapi import EpsonPrinterAPI
 import voluptuous as vol
 
+from homeassistant.components.sensor import (
+    PLATFORM_SCHEMA,
+    SensorEntity,
+    SensorEntityDescription,
+)
+from homeassistant.const import CONF_HOST, CONF_MONITORED_CONDITIONS, PERCENTAGE
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import PlatformNotReady
 import homeassistant.helpers.config_validation as cv
-from homeassistant.helpers.entity import Entity
-from homeassistant.components.sensor import PLATFORM_SCHEMA
-from homeassistant.const import (
-    CONF_NAME, CONF_HOST, CONF_MONITORED_CONDITIONS)
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
-REQUIREMENTS = ['epsonprinter==0.0.1']
+SENSOR_TYPES: tuple[SensorEntityDescription, ...] = (
+    SensorEntityDescription(
+        key="black",
+        name="Ink level Black",
+        icon="mdi:water",
+        native_unit_of_measurement=PERCENTAGE,
+    ),
+    SensorEntityDescription(
+        key="photoblack",
+        name="Ink level Photoblack",
+        icon="mdi:water",
+        native_unit_of_measurement=PERCENTAGE,
+    ),
+    SensorEntityDescription(
+        key="magenta",
+        name="Ink level Magenta",
+        icon="mdi:water",
+        native_unit_of_measurement=PERCENTAGE,
+    ),
+    SensorEntityDescription(
+        key="cyan",
+        name="Ink level Cyan",
+        icon="mdi:water",
+        native_unit_of_measurement=PERCENTAGE,
+    ),
+    SensorEntityDescription(
+        key="yellow",
+        name="Ink level Yellow",
+        icon="mdi:water",
+        native_unit_of_measurement=PERCENTAGE,
+    ),
+    SensorEntityDescription(
+        key="clean",
+        name="Cleaning level",
+        icon="mdi:water",
+        native_unit_of_measurement=PERCENTAGE,
+    ),
+)
+MONITORED_CONDITIONS: list[str] = [desc.key for desc in SENSOR_TYPES]
 
-_LOGGER = logging.getLogger(__name__)
-
-DEFAULT_IP = '127.0.0.1'
-
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_HOST): cv.string,
+        vol.Required(CONF_MONITORED_CONDITIONS): vol.All(
+            cv.ensure_list, [vol.In(MONITORED_CONDITIONS)]
+        ),
+    }
+)
 SCAN_INTERVAL = timedelta(minutes=60)
 
-MONITORED_CONDITIONS = {
-    'black': ['Inklevel Black', '%', 'mdi:water'],
-    'magenta': ['Inklevel Magenta', '%', 'mdi:water'],
-    'cyan': ['Inklevel Cyan', '%', 'mdi:water'],
-    'yellow': ['Inklevel Yellow', '%', 'mdi:water'],
-    'clean': ['Inklevel Cleaning', '%', 'mdi:water'],
-}
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Required(CONF_HOST, default=DEFAULT_IP): cv.string,
-    vol.Required(CONF_MONITORED_CONDITIONS, default=MONITORED_CONDITIONS):
-        vol.All(cv.ensure_list, [vol.In(MONITORED_CONDITIONS)]),
-})
-
-
-def setup_platform(hass, config, add_devices, discovery_info=None):
+def setup_platform(
+    hass: HomeAssistant,
+    config: ConfigType,
+    add_devices: AddEntitiesCallback,
+    discovery_info: DiscoveryInfoType | None = None,
+) -> None:
     """Set up the cartridge sensor."""
     host = config.get(CONF_HOST)
-    from epsonprinter import EpsonPrinter
-    """Set up the printer API."""
-    api = EpsonPrinter(host)
 
-    sensors = [EpsonPrinterCartridge(hass, api, condition)
-               for condition in config[CONF_MONITORED_CONDITIONS]]
+    api = EpsonPrinterAPI(host)
+    if not api.available:
+        raise PlatformNotReady()
+
+    sensors = [
+        EpsonPrinterCartridge(api, description)
+        for description in SENSOR_TYPES
+        if description.key in config[CONF_MONITORED_CONDITIONS]
+    ]
 
     add_devices(sensors, True)
 
 
-class EpsonPrinterCartridge(Entity):
-    """Representation of a cartdige sensor."""
+class EpsonPrinterCartridge(SensorEntity):
+    """Representation of a cartridge sensor."""
 
-    def __init__(self, hass, api,  variable):
+    def __init__(
+        self, api: EpsonPrinterAPI, description: SensorEntityDescription
+    ) -> None:
         """Initialize a cartridge sensor."""
-        self._hass = hass
         self._api = api
-
-        variable_info = MONITORED_CONDITIONS[variable]
-        self._var_name = variable_info[0]
-        self._var_id = variable;
-        self._var_unit = variable_info[1]
-        self._var_icon = variable_info[2]
+        self.entity_description = description
 
     @property
-    def name(self):
-        """Return the name of the sensor."""
-        return self._var_name
-
-    @property
-    def icon(self):
-        """Icon to use in the frontend, if any."""
-        return self._var_icon
-
-    @property
-    def unit_of_measurement(self):
-        """Return the unit the value is expressed in."""
-        return self._var_unit
-
-    @property
-    def state(self):
+    def native_value(self):
         """Return the state of the device."""
-        return self._api.getSensorValue(self._var_id)
+        return self._api.getSensorValue(self.entity_description.key)
 
     @property
-    def available(self):
+    def available(self) -> bool:
         """Could the device be accessed during the last update call."""
         return self._api.available
 
-    def update(self):
+    def update(self) -> None:
         """Get the latest data from the Epson printer."""
         self._api.update()
-
